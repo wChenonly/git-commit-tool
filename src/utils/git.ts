903f3570e032db84451fe5ea4c8e9fd89d34ba22
wChenonly <wChenonly@qq.com>
2023-08-09 10:26:33 +0800

    🚑 bugfix: 修复execa catch情况

--- src/utils/git.ts
+++ src/utils/git.ts
@@ -5,14 +5,20 @@ import { HAND_ICON } from './utils'
 
 export async function commit(message: string) {
   const spinner = ora({ text: 'git commit ...', stream: process.stdout }).start()
-  await execa('git', ['commit', '-m', message], { stdio: 'inherit' })
+  await execa('git', ['commit', '-m', message], { stdio: 'inherit' }).catch(() => {
+    spinner.fail(`${c.bgRed('请修复 lint 错误')}`)
+    process.exit(1)
+  })
   spinner.succeed(`${c.green('git commit succeed 🍺 🍺 🍺')}`)
 }
 
 export async function push(currentBranch: string) {
   const spinner = ora('git push ...').start()
   try {
-    await execa('git', ['push', 'origin', `${currentBranch}`])
+    await execa('git', ['push', 'origin', `${currentBranch}`]).catch(() => {
+      spinner.fail(`${c.bgRed(`推送失败，请手动 git push ${HAND_ICON}`)}`)
+      process.exit(1)
+    })
     spinner.succeed(`${c.green('git push succeed 💯 💯 💯')}`)
     return true
   }
